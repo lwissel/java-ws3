9f5e4e4dd14c6a9f994a88cfa104e89e5029bbb0
lwissel <lennart.wissel@gmx.net>
2014-10-28 23:04:19 +0000

    add length counter

---
+++ Ex3.java
@@ -0,0 +1,20 @@
+/** Exercise 3 ws3
+ *
+ * Calculates the total digits su of an integer
+ *
+ * first non-recursively, then recursive
+ */
+
+public class Ex3 {
+  public static void main(String[] args) {
+
+  }
+
+  public static int digitTotalIter(int n) {
+    // get the number of digits
+    int length = Integer.toString(n).length();
+
+
+    return length;
+  }
+}
