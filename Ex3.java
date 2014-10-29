8bc1e2d5b756998e674dd5ddc7ba6d017d17089d
lwissel <lennart.wissel@gmx.net>
2014-10-29 12:46:50 +0000

    add recursive version

--- Ex3.java
+++ Ex3.java
@@ -10,11 +10,53 @@ public class Ex3 {
 
   }
 
+  /** method to calculate the digitTotal of a given integer
+   * using a string object to avoid the use of % operations
+   * @param n integer to calculate the digitTotal of
+   * @return integer representing the digitTotal of n
+   */
   public static int digitTotalIter(int n) {
-    // get the number of digits
-    int length = Integer.toString(n).length();
+    // convert the int to string for easier operations, only allow positives
+    String s = Integer.toString(Math.abs(n));
 
-
-    return length;
+    int res = 0;
+    
+    int i = 0;
+    while(i < s.length()) {
+      res += Integer.parseInt(s.charAt(i)+""); // note that +"" is needed to convert char to string
+      i++;
+    }
+    return res;
   }
+
+
+  /** RECURSIVE  method to calculate the digitTotal of a given integer
+   * using a string object to avoid the use of % operations
+   * @param n integer to calculate the digitTotal of
+   * @return integer representing the digitTotal of n
+   */
+  public static int digitTotalRec(int n) {
+    return digitTotalRec(n,0);
+  }
+
+
+  /** RECURSIVE helper method to calculate the digitTotal of a given integer
+   * using a string object to avoid the use of % operations
+   * @param n integer to calculate the digitTotal of
+   * @param pos is the recursion control parameter
+   * @return integer representing the digitTotal of n
+   */
+  public static int digitTotalRec(int n, int pos) {
+    // convert the int to string for easier operations, only allow positives
+    String s = Integer.toString(Math.abs(n));
+
+    // our break condition is when are out of bounds
+    if (pos == s.length()) {
+      return 0;
+    }
+    else {
+      return Integer.parseInt(s.charAt(pos)+"") + digitTotalRec(n,pos+1);
+    }
+  }
+
 }
