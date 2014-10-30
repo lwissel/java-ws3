68898526bb706683f047ae89e7bb6bcb35ad1c00
lwissel <lennart.wissel@gmx.net>
2014-10-30 17:23:53 +0000

    add basic file construct

---
+++ Ex4.java
@@ -0,0 +1,23 @@
+/** Exercise 4 ws3
+ *
+ * implementing the quicksort algorithm
+ *
+ * using two subarrays the elements are presorted by comparing each element with a given 'pivot' element
+ * the pivot should be chosen that both subarrays should be of the same length
+ *
+ * @author  Lennart Wissel
+ * @version 29.10.2014
+ */
+
+public class Ex4 {
+  public static void main(String[] args) {
+  }
+
+  /** method to sort a string array according to the string length using the
+   * quicksort algorithm
+   * @param s string array to sort
+   * @return sorted string in increasing string length
+   */
+  public static String[] quickSort(String[] s) {
+  }
+}
