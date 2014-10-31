987fd53486c28856cd4d7e1aa3b07848e89a547b
lwissel <lennart.wissel@gmx.net>
2014-10-31 11:31:12 +0000

    add swap and isSorted

--- Ex4.java
+++ Ex4.java
@@ -16,8 +16,46 @@ public class Ex4 {
   /** method to sort a string array according to the string length using the
    * quicksort algorithm
    * @param s string array to sort
-   * @return sorted string in increasing string length
+   * @return sorted array in increasing order
    */
   public static String[] quickSort(String[] s) {
+    return s;
   }
+
+  /** HELPER METHODS */
+
+  /** swaps to elements in string array
+   * @param array array to operate on
+   * @param n position 1
+   * @param m position 2
+   * @return array with elements n,m swapped
+   */
+  public static String[] swap(String[] array, int n, int m) {
+    if (n >= array.length || m >= array.length) { return array; }
+
+    String tmp = array[n];
+    array[n] = array[m];
+    array[m] = tmp;
+
+    return array;
+  }
+
+  /** check if given string array is sorted in raising order
+   * @param array string array to check
+   * @return true if sorted, false if not
+   */
+  public static boolean isSorted(String[] array) {
+    if (array.length == 0 || array.length == 1) { return true; }
+    boolean result = false;
+    for(int i = 0; i < array.length-1; i++) {
+      if (array[i].length() <= array[i+1].length()) {
+        result = true;
+      }
+      else {
+        return false;
+      }
+    }
+    return result;
+  }
+
 }
